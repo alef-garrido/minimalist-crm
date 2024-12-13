2c6fa15cbebbe0136d2988b39ef1dfe245d4f2b7
unknown <oa.garrido11@gmail.com>
2024-12-13 17:54:44 -0600

    Add Instruction line to header section

--- app/components/welcomePage.tsx
+++ app/components/welcomePage.tsx
@@ -14,7 +14,7 @@ const WelcomePage: React.FC = () => {
     <main className="flex items-center justify-center pt-16 pb-4">
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
         <header className="flex flex-col items-center gap-9">
-          <h1>Hey there!</h1>
+          <h1 className="text-slate-700">INSTRUCCIONES: Ingresa los datos de tu contacto y revisalos en la lista que aparece debajo.</h1>
         </header>
         <div>
       <h1 className="text-2xl font-bold text-center my-4">CRM App</h1>
